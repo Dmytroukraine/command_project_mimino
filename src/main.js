6d86c1300a216b4d266834e96ce208193d546ee1
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-02-28 21:28:19 +0200

    add js for modal win

--- src/main.js
+++ src/main.js
@@ -12,3 +12,33 @@
       refs.menu.classList.toggle('is-open');
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open2]"),
+      closeModalBtn: document.querySelector("[data-modal-close2]"),
+      modal: document.querySelector("[data-modal2]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden2");
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
