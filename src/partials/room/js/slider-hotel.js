633c2e1052d5df439844ad899d521dc5f05cc628
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-03-02 00:37:01 +0200

    Update for team
    I hope it will work...

---
+++ src/partials/room/js/slider-hotel.js
@@ -0,0 +1,15 @@
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
---
+++ src/partials/room/js/slider.js
@@ -0,0 +1,9 @@
+
+$('.autoplay').slick({
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dotsClass: "my-dots",
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
