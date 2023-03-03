a8b181def848c1875c5c123dd5a8f71896b68817
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-03-03 22:31:59 +0200

    html/css add import and load for hotel/restaurant/room

---
+++ src/main-hotel.js
@@ -0,0 +1,44 @@
+(() => {
+    const refs = {
+      openMenuBtn: document.querySelector('[data-menu-open]'),
+      closeMenuBtn: document.querySelector('[data-menu-close]'),
+      menu: document.querySelector('[data-menu]'),
+    };
+  
+    refs.openMenuBtn.addEventListener('click', toggleModal);
+    refs.closeMenuBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.menu.classList.toggle('is-open');
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open2]"),
+      closeModalBtn: document.querySelector("[data-modal-close2]"),
+      modal: document.querySelector("[data-modal2]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden2");
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
