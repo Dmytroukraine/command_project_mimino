090241e2c7af2dcbccca8985da6a6eb1d6ad2f87
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-03-08 01:41:10 +0200

    WTF

--- src/main_restaurant.js
+++
@@ -1,6 +0,0 @@
-import './js/mobile-menu'
-import './js/header'
-import './js/modals'
-import './js/scrol'
-import './js/slider_restaurant-menu'
-
--- src/main_room.js
+++
@@ -1,5 +0,0 @@
-import './js/mobile-menu'
-import './js/header'
-import './js/modals'
-import './js/scrol'
-import './js/slider'
